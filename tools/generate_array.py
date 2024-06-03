e782ae1b0dc623a3f3bf331f8e69ec5b680dd1f4
shapedb <jsilvanmendez@gmail.com>
2024-06-03 09:25:25 -0600

    Proyecto Completado

--- tools/generate_array.py
+++ tools/generate_array.py
@@ -4,7 +4,3 @@ import random
 def generar_array_sin_repeticion(max_mount,limit):
     random_mount = random.sample(range(1, max_mount + 1), max_mount)
     return random_mount[:limit]
-
-
-for i, dato in enumerate(generar_array_sin_repeticion(100,10)):
-    print(dato)
